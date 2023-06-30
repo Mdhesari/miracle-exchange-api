3480a99f9c2d4ca627452d03a15dd1868a33552d
Mohamad Hesari <mdhesari99@gmail.com>
2023-06-30 18:25:11 +0330

    only active gateays for ordinary users

--- Modules/Gateway/Actions/ApplyGatewayQueryFilters.php
+++ Modules/Gateway/Actions/ApplyGatewayQueryFilters.php
@@ -10,8 +10,12 @@ class ApplyGatewayQueryFilters extends BaseQueryFilters
     {
         parent::__invoke($query, $data);
 
-        if ( isset($data['is_active']) ) {
-            $query->where('is_active', $data['is_active']);
+        if ( $this->user()->cannot('gateways') ) {
+            $query->where('is_active', true);
+        } else {
+            if ( isset($data['is_active']) ) {
+                $query->where('is_active', $data['is_active']);
+            }
         }
 
         return $query;
