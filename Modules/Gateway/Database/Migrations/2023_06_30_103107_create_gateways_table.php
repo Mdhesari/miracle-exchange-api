daa036c0d0b484afae75ccc5b9074f7f77ac79b9
Mohamad Hesari <mdhesari99@gmail.com>
2023-07-06 14:38:54 +0330

    fix uuid gatways

--- Modules/Gateway/Database/Migrations/2023_06_30_103107_create_gateways_table.php
+++ Modules/Gateway/Database/Migrations/2023_06_30_103107_create_gateways_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('gateways', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
 
             $table->string('title');
             $table->string('sheba_number');
