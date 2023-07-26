154147de4818e7d4473463006426159f281d6aa9
Mohamad Hesari <mdhesari99@gmail.com>
2023-07-26 14:19:11 +0330

    account sheba is nullable

--- Modules/Account/Http/Requests/AccountRequest.php
+++ Modules/Account/Http/Requests/AccountRequest.php
@@ -15,7 +15,7 @@ class AccountRequest extends FormRequest
     {
         return [
             'title'          => 'required|string',
-            'sheba_number'   => 'required|string',
+            'sheba_number'   => 'nullable|string',
             'account_number' => 'required|string',
             'account_name'   => 'required|string',
             'user_id'        => 'nullable|exists:users,id',
