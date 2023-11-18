3e27e7bf4d1ea681b7b71609068fe8c709dca721
Mohamad Hesari <mdhesari99@gmail.com>
2023-11-18 16:05:44 +0330

    sitemap generator

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('market:update-navasan')->everyTwoMinutes();
+        $schedule->command('sitemap:generate')->daily();
     }
 
     /**
