1494b6834793e1cc903ae6eb7f5034355d997f77
Mohamad Hesari <mdhesari99@gmail.com>
2024-01-28 00:46:24 +0330

    feat(notification): new order notif to admins

--- Modules/Order/Listeners/SendOrderNotifications.php
+++ Modules/Order/Listeners/SendOrderNotifications.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Order\Listeners;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Modules\Auth\Jobs\SendSMS;
 use Modules\Order\Events\OrderCreated;
@@ -28,14 +29,26 @@ class SendOrderNotifications
     {
         $order = $event->order;
 
-        if ($order->user->mobile)
+        if ($order->user->mobile) {
             SendSMS::dispatch($order->user->mobile, 'submitOrder', [
-                // amount
+                // price
                 $order->fromatted_cumulative_quote_quantity.'_تومان',
                 // currency
                 Str::replace(' ', '_', $order->market->persian_name),
-                // currency price
+                // quantity
                 $order->formatted_executed_quantity,
             ]);
+        }
+
+        foreach (User::permission('order')->whereNotNull('mobile')->cursor() as $user) {
+            SendSMS::dispatch($user->mobile, 'adminSubmitOrder', [
+                // currency
+                Str::replace(' ', '_', $order->market->persian_name),
+                // quantity
+                $order->formatted_executed_quantity,
+                // price
+                $order->fromatted_cumulative_quote_quantity.'_تومان',
+            ]);
+        }
     }
 }
