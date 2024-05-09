c3e541499742d93a40c27d0dfe0eca1328299fd9
Mohamad Hesari <mdhesari99@gmail.com>
2024-05-10 00:16:08 +0330

    fix: market chart

--- Modules/Market/Http/Controllers/MarketChartController.php
+++ Modules/Market/Http/Controllers/MarketChartController.php
@@ -15,13 +15,13 @@ class MarketChartController extends Controller
 
     public function __invoke(Market $market)
     {
-        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "%Y-%m-%d %H:00:00") AS session_hour, price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf") AS subquery'))
+        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "%Y-%m-%d 00:00:00") AS session_hour, price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf") AS subquery'))
             ->select([
                 DB::raw('session_hour'),
                 DB::raw('MAX(price) AS high_price'),
                 DB::raw('MIN(price) AS low_price'),
-                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date ASC LIMIT 1) AS open_price'),
-                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date DESC LIMIT 1) AS close_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d 00:00:00") = session_hour ORDER BY date ASC LIMIT 1) AS open_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d 00:00:00") = session_hour ORDER BY date DESC LIMIT 1) AS close_price'),
             ])
             ->groupBy('session_hour')
             ->orderBy('session_hour')->get();
