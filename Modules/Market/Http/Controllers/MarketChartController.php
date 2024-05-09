00d918fd2fd063609fd876bd14217f098ae1bd9e
Mohamad Hesari <mdhesari99@gmail.com>
2024-05-10 00:15:04 +0330

    fix: market chart

--- Modules/Market/Http/Controllers/MarketChartController.php
+++ Modules/Market/Http/Controllers/MarketChartController.php
@@ -15,35 +15,17 @@ class MarketChartController extends Controller
 
     public function __invoke(Market $market)
     {
-//        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "%Y-%m-%d %H:00:00") AS session_hour, price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf") AS subquery'))
-//            ->select([
-//                DB::raw('session_hour'),
-//                DB::raw('MAX(price) AS high_price'),
-//                DB::raw('MIN(price) AS low_price'),
-//                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date ASC LIMIT 1) AS open_price'),
-//                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date DESC LIMIT 1) AS close_price'),
-//            ])
-//            ->groupBy('session_hour')
-//            ->orderBy('session_hour', 'asc')->get();
-//        dd($results->toSql());
+        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "%Y-%m-%d %H:00:00") AS session_hour, price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf") AS subquery'))
+            ->select([
+                DB::raw('session_hour'),
+                DB::raw('MAX(price) AS high_price'),
+                DB::raw('MIN(price) AS low_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date ASC LIMIT 1) AS open_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "9bfdca97-b26a-4811-8cd9-96edab4fc7bf" AND DATE_FORMAT(date, "%Y-%m-%d %H:00:00") = session_hour ORDER BY date DESC LIMIT 1) AS close_price'),
+            ])
+            ->groupBy('session_hour')
+            ->orderBy('session_hour')->get();
 
-        $results = DB::select("SELECT
-    session_hour,
-    MAX(price) AS high_price,
-    MIN(price) AS low_price,
-    (SELECT price FROM market_prices WHERE market_id = '?' AND DATE_FORMAT(date, '%Y-%m-%d %H:00:00') = session_hour ORDER BY date ASC LIMIT 1) AS open_price,
-    (SELECT price FROM market_prices WHERE market_id = '?' AND DATE_FORMAT(date, '%Y-%m-%d %H:00:00') = session_hour ORDER BY date DESC LIMIT 1) AS close_price
-FROM (
-    SELECT DATE_FORMAT(date, '%Y-%m-%d %H:00:00') AS session_hour, price
-    FROM market_prices
-    WHERE market_id = '?'
-) AS subquery
-GROUP BY session_hour
-ORDER BY session_hour ASC;", ["9bfdca97-b26a-4811-8cd9-96edab4fc7bf", "9bfdca97-b26a-4811-8cd9-96edab4fc7bf", "9bfdca97-b26a-4811-8cd9-96edab4fc7bf"]);
-
-// $results now contains the result set
-
-        dd($results);
         return api()->success(null, [
             'items' => $results,
         ]);
