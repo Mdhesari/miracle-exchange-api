1ea2764f5354d71a6865a3c0f3e24cfc63a8bb5e
Mohamad Hesari <mdhesari99@gmail.com>
2024-05-10 13:29:23 +0330

    fix: market chart dateformat

--- Modules/Market/Http/Controllers/MarketChartController.php
+++ Modules/Market/Http/Controllers/MarketChartController.php
@@ -16,13 +16,13 @@ class MarketChartController extends Controller
     public function __invoke(Market $market)
     {
         $dateFormat = "%Y-%m-%d %H:00:00";
-        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, '.$dateFormat.') AS datetime, price FROM market_prices WHERE market_id = "'.$market->id.'") AS subquery'))
+        $results = DB::table(DB::raw('(SELECT DATE_FORMAT(date, "'.$dateFormat.'") AS datetime, price FROM market_prices WHERE market_id = "'.$market->id.'") AS subquery'))
             ->select([
                 DB::raw('datetime'),
                 DB::raw('MAX(price) AS high_price'),
                 DB::raw('MIN(price) AS low_price'),
-                DB::raw('(SELECT price FROM market_prices WHERE market_id = "'.$market->id.'" AND DATE_FORMAT(date, '.$dateFormat.') = datetime ORDER BY date ASC LIMIT 1) AS open_price'),
-                DB::raw('(SELECT price FROM market_prices WHERE market_id = "'.$market->id.'" AND DATE_FORMAT(date, '.$dateFormat.') = datetime ORDER BY date DESC LIMIT 1) AS close_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "'.$market->id.'" AND DATE_FORMAT(date, "'.$dateFormat.'") = datetime ORDER BY date ASC LIMIT 1) AS open_price'),
+                DB::raw('(SELECT price FROM market_prices WHERE market_id = "'.$market->id.'" AND DATE_FORMAT(date, "'.$dateFormat.'") = datetime ORDER BY date DESC LIMIT 1) AS close_price'),
             ])
             ->groupBy('datetime')
             ->orderBy('datetime')->get();
