f67c081a3b59db759fc7d2095bf7d91712866ad2
Mohamad Hesari <mdhesari99@gmail.com>
2024-05-15 00:35:30 +0330

    feat: market seeder to=man

--- Modules/Market/Database/Seeders/MarketDatabaseSeeder.php
+++ Modules/Market/Database/Seeders/MarketDatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Modules\Market\Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 use Modules\Market\Entities\Market;
+use Modules\Market\Enums\MarketStatus;
 
 class MarketDatabaseSeeder extends Seeder
 {
@@ -16,5 +17,15 @@ class MarketDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        Market::create([
+            'name'         => 'toman',
+            'persian_name' => 'تومان',
+            'country_code' => 'ir',
+            'symbol_char'  => 'T',
+            'symbol'       => 'toman',
+            'price'        => 1,
+            'status'       => MarketStatus::Enabled->name,
+        ]);
     }
 }
